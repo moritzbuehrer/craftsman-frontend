9adc0d0f9aea6a796a55ad76f4d202e41cf6b935
mbuehrer <Moritz96>
2020-08-22 02:13:10 +0200

    data clean up

--- src/reducers/project.js
+++ src/reducers/project.js
@@ -7,20 +7,7 @@ const initialState = {
         name: "Kernsanierung",
         description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         customerId: "1",
-        timeTracks: [
-            {
-                id: "1",
-                employee: "Moritz",
-                workingTime: "12",
-                note: "Test"
-            },
-            {
-                id: "2",
-                employee: "MC",
-                workingTime: "19",
-                note: "Test Note"
-            }
-        ]
+        timeTracks: []
     },
     projects: [],
     loading: false,
